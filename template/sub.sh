f3437cbb37e880a8c6d1074680e56f31daf95ddf
Koichi Murase <myoga.murase@gmail.com>
2020-09-08 17:38:55 +0800

    idtsub/stat: measure job time

--- template/sub.sh
+++ template/sub.sh
@@ -8,6 +8,7 @@ print_var() { local q=\' Q="'\''"; printf "%s='%s'\n" "$1" "${2//$q/$Q}" >> "$ID
 print_log 'IDTSUB run' 
 print_var node "$HOSTNAME"
 print_var stat R
+print_var time_start "$(date +%s)"
 if [[ $IDTSUB_TMPDIR ]]; then
   export IDTSUB_TMPDIR
   [[ -d $IDTSUB_TMPDIR ]] ||
@@ -17,4 +18,5 @@ srun -n 1 "$IDTSUB_JOBDIR/job.sh"
 [[ $IDTSUB_TMPDIR ]] && rm -rf "$IDTSUB_TMPDIR"
 print_log 'IDTSUB complete'
 print_var stat C
+print_var time_end "$(date +%s)"
 echo 1 >> "$IDTSUB_JOBDIR/complete.mark" 
